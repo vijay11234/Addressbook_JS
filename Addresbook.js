0f6cb801d8bb3880adaa995281f0d74b4cb78cc2
vijay11234 <victory6923@gmail.com>
2020-12-07 09:58:19 +0530

    Contacts sorted by stated and zip

--- Addresbook.js
+++ Addresbook.js
@@ -25,6 +25,8 @@ class AddressBook {
       throw "Invalid first Name";
   }
 
+  //get and set for lastname
+  //first letter should be capital and min 3 letters
   get lastName() { return this._lastName; }
   set lastName(lastName) {
     let nameRegex = RegExp("^[A-Z]{1}[A-Za-z]{2,}$");
@@ -35,6 +37,8 @@ class AddressBook {
       throw "Invalid last Name";
   }
 
+  //get and set for address
+  //minimum four characters
   get address() { return this._address; }
   set address(address) {
     let addressRegex = RegExp("^[A-Za-z]{4,}$");
@@ -44,6 +48,8 @@ class AddressBook {
       throw "Invalid address ";
   }
 
+  //get and set for city
+  //minimum four characters
   get city() { return this._city; }
   set city(city) {
     let cityRegex = RegExp("^[A-Za-z]{4,}$");
@@ -52,7 +58,7 @@ class AddressBook {
     else
       throw "Invalid city ";
   }
-
+  
   get state() { return this._state; }
   set state(state) {
     let stateRegex = RegExp("^[A-Za-z0-9]{4,}$");
@@ -65,7 +71,7 @@ class AddressBook {
   //pin code of form 789 987
   get zip() { return this._zip; }
   set zip(zip) {
-    let zipRegex = RegExp("^[1-9]{3}[ ]?[0-9]{3}$");
+    let zipRegex = RegExp("^[1-9]{3}[ ]*[0-9]{3}$");
     if (zipRegex.test(zip))
       this._zip = zip;
     else
@@ -92,6 +98,7 @@ class AddressBook {
       throw "Invalid email";
   }
 
+  //defining to string method
   toString() {
     return "first Name: " + this.firstName + ", last Name: " + this.lastName
       + "\nAddress: " + this.address + ", City: " + this.city
@@ -103,11 +110,11 @@ class AddressBook {
 try {
   // uc3 adding contact details to array 
   let contactDetailsArray = new Array();
-  contactDetailsArray.push(new AddressBook("Vijaya", "Kumar", "Manvi", "Raichur", "Karnataka", "421003",
+  contactDetailsArray.push(new AddressBook("Vijay", "Kumar", "Manvi", "Raichur", "Karnataka", "421004",
     "91 7558368932", "bharambeakshay@gmail.com"));
-  contactDetailsArray.push(new AddressBook("Arun", "Kumar", "nearhighway", "Raichur", "Karnataka",
+  contactDetailsArray.push(new AddressBook("Arun", "Machnoor", "nearhighway", "Raichur", "Karnataka",
     "128 755", "91 8587087642", "roma@gmail.com"));
-  contactDetailsArray.push(new AddressBook("Pallavi", "Store", "fourstreet", "Landan", "France",
+  contactDetailsArray.push(new AddressBook("Daisy", "Alava", "fourstreet", "Paris", "France",
     "871 258", "87 6587321451", "daisy@gmail.com"));
   contactDetailsArray.push(new AddressBook("Troy", "Tom", "alaska", "Zurich", "Switzerland",
     "128 213", "91 8966541252", "troy@gmail.com"));
@@ -115,20 +122,23 @@ try {
   contactDetailsArray.forEach((contact) => console.log(contact.toString()));
 
   // uc4 finding index using name
+  console.log("****************************************************************");
   let index = contactDetailsArray.findIndex(contact => contact.firstName == "Arun");
   //updating the contact detail
-  contactDetailsArray[index].zip = "121 003";
+  contactDetailsArray[index].zip = "121003";
 
   //displaying contacts after being updated
   console.log("contacts after being updated");
   contactDetailsArray.forEach((contact) => console.log(contact.toString()));
 
   //uc5 Removes an element from an array at specified index
+  console.log("******************************************************************");
   contactDetailsArray.splice(index, 1);
   console.log("contacts after being deleted");
   contactDetailsArray.forEach((contact) => console.log(contact.toString()));
 
   //uc6 Reduce function to find number of contacts
+  console.log("*******************************************************************");
   var totalContacts = 0;
   function FindTotalContacts(contactDetailsArray) {
     if (contactDetailsArray != null)
@@ -141,7 +151,7 @@ try {
   //uc7 check for duplicate value
   let countForDuplicate = 0;
   function CheckForDuplicates(contact) {
-    if (contact.firstName == "Vijaya")
+    if (contact.firstName == "Vijay")
       countForDuplicate++;
     return countForDuplicate;
   }
@@ -157,7 +167,7 @@ try {
   var numberOfCity = 0;
   var numberOfState = 0;
   console.log("");
-  console.log("Contact by City Landan")
+  console.log("Contact by City Paris")
   function ContactByCity(contactDetails) {
     if (contactDetails.city == "Paris") {
       console.log(contactDetails.toString());
@@ -195,11 +205,38 @@ try {
   console.log(addressStateMap);
 
   //uc11 sorting by name and printing the array
-  console.log("Sorting by firstName");
-  for (let contactDetails in contactDetailsArray) {
-    contactDetailsArray.sort(contactDetails.firstName);
+  function SortByName() {
+    for (let contactDetails in contactDetailsArray) {
+      contactDetailsArray.sort(contactDetails.firstName);
+    }
+    contactDetailsArray.forEach((contact) => console.log(contact.toString()));
   }
-  contactDetailsArray.forEach((contact) => console.log(contact.toString()));
+  function SortByCity() {
+    for (let contactDetails in contactDetailsArray) {
+      contactDetailsArray.sort(contactDetails.city);
+    }
+    contactDetailsArray.forEach((contact) => console.log(contact.toString()));
+  }
+  function SortByState() {
+    for (let contactDetails in contactDetailsArray) {
+      contactDetailsArray.sort(contactDetails.state);
+    }
+    contactDetailsArray.forEach((contact) => console.log(contact.toString()));
+  }
+  function SortByZip() {
+    for (let contactDetails in contactDetailsArray) {
+      contactDetailsArray.sort(contactDetails.zip);
+    }
+    contactDetailsArray.forEach((contact) => console.log(contact.toString()));
+  }
+  console.log("Sorting by firstName");
+  SortByName();
+  console.log("Sort By City")
+  SortByCity();
+  console.log("Sort By State")
+  SortByState();
+  console.log("Sort By Zip")
+  SortByZip();
 }
 catch (e) {
   console.log(e);
